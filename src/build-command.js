6c3f7d577718188f684de7ee6270a15bfb147249
Yuan Qing <desk@yuanqing.sg>
2018-04-30 02:56:33 +0800

    Fix piping in from `stdin`

--- src/build-command.js
+++ src/build-command.js
@@ -2,6 +2,7 @@ const promisify = require('util').promisify
 const execa = require('execa')
 const path = require('path')
 const mkdirp = promisify(require('mkdirp'))
+const uniqueSlug = require('unique-slug')
 
 function stringifyOptions (options) {
   const result = []
@@ -24,10 +25,11 @@ function stringifyOptions (options) {
   return `${result.join(' ')} -filter_complex "[0:v] ${filters.join(',')}"`
 }
 
-function buildOutputFile (inputFile, outputDirectory, isGif) {
-  if (isGif) {
-    const inputFileParentDirectory = path.dirname(inputFile)
+async function buildOutputFile (inputFile, outputDirectory, convertToGif) {
+  inputFile = inputFile === '-' ? `${uniqueSlug()}.mov` : inputFile
+  if (convertToGif) {
     const inputFileExtension = path.extname(inputFile)
+    const inputFileParentDirectory = path.dirname(inputFile)
     const outputFile = `${path.basename(inputFile, inputFileExtension)}.gif`
     return path.join(outputDirectory, inputFileParentDirectory, outputFile)
   }
@@ -36,12 +38,22 @@ function buildOutputFile (inputFile, outputDirectory, isGif) {
 
 function buildCommand (inputFile, outputDirectory, convertToGif, options) {
   const stringifiedOptions = stringifyOptions(options)
-  const outputFile = buildOutputFile(inputFile, outputDirectory, convertToGif)
-  const outputFileParentDirectory = path.resolve(outputFile, '..')
-  const ffmpegCommand = `ffmpeg -y -i ${inputFile} ${stringifiedOptions} -- ${outputFile}`
-  return async function () {
-    await mkdirp(outputFileParentDirectory)
-    return execa.shell(ffmpegCommand, { stdio: ['pipe', 'ignore', 'ignore'] })
+  return function () {
+    return new Promise(async function (resolve) {
+      const outputFile = await buildOutputFile(inputFile, outputDirectory, convertToGif)
+      const outputFileParentDirectory = path.resolve(outputFile, '..')
+      const ffmpegCommand = `ffmpeg -y -i ${inputFile} ${stringifiedOptions} -- ${outputFile}`
+      await mkdirp(outputFileParentDirectory)
+      const ps = execa.shell(ffmpegCommand)
+      if (inputFile === '-') {
+        ps.on('close', function (code) {
+          resolve(ps)
+        })
+        process.stdin.pipe(ps.stdin)
+        return
+      }
+      resolve(ps)
+    })
   }
 }
 
