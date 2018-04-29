d4e584561019ab66e16495d8ac842c6034e05a89
Yuan Qing <desk@yuanqing.sg>
2018-04-29 20:37:46 +0800

    Add `crop`

--- src/methods/crop.js
+++ src/methods/crop.js
@@ -1,5 +1,8 @@
-function crop () {
-  return {}
+function crop (x, y, width, height) {
+  // https://video.stackexchange.com/a/4571
+  return {
+    vf: `crop=${width}:${height}:${x}:${y}`
+  }
 }
 
 module.exports = crop
