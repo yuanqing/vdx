de48b43c2c33eec9d9d366d0502e6e14f480b77b
Yuan Qing <desk@yuanqing.sg>
2018-04-30 02:55:58 +0800

    Return empty object

--- src/methods/gif.js
+++ src/methods/gif.js
@@ -1,5 +1,8 @@
 function gif (fps) {
   // https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+  if (fps === -1) {
+    return {}
+  }
   return {
     filter_complex: `fps=${fps},split [a][b];[a] palettegen [palette];[b][palette] paletteuse`
   }
