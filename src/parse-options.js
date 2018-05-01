e21fd996c70ff6078b14f8abbf2db0ad9373992e
Yuan Qing <desk@yuanqing.sg>
2018-05-02 01:27:10 +0800

    Add `speed`

--- src/parse-options.js
+++ src/parse-options.js
@@ -1,16 +1,19 @@
 const parseTrim = require('./parse-trim')
 
 function parseOptions (options) {
-  const { crop, format, fps, noAudio, resize, trim } = options
+  const { crop, format, fps, noAudio, resize, speed, trim } = options
   const result = []
-  const filters = []
+  const videoFilters = []
+  const audioFilters = []
 
-  if (noAudio || format === 'gif') {
-    // https://superuser.com/a/268986
-    result.push({ c: 'copy', an: true })
-  } else {
-    // https://superuser.com/q/602983
-    result.push({ 'c:a': 'copy' })
+  if (!speed) {
+    if (noAudio || format === 'gif') {
+      // https://superuser.com/a/268986
+      result.push({ c: 'copy', an: true })
+    } else {
+      // https://superuser.com/q/602983
+      result.push({ 'c:a': 'copy' })
+    }
   }
 
   if (fps) {
@@ -19,28 +22,37 @@ function parseOptions (options) {
   }
 
   if (trim) {
-    result.push(parseTrim(trim))
+    result.push(parseTrim(trim, speed))
   }
 
   // https://superuser.com/a/624564
   if (resize) {
-    filters.push(`scale=${resize.width}:${resize.height}`)
+    videoFilters.push(`scale=${resize.width}:${resize.height}`)
   }
 
   // https://video.stackexchange.com/a/4571
   if (crop) {
-    filters.push(`crop=${crop.width}:${crop.height}:${crop.x}:${crop.y}`)
+    videoFilters.push(`crop=${crop.width}:${crop.height}:${crop.x}:${crop.y}`)
+  }
+
+  // http://trac.ffmpeg.org/wiki/How%20to%20speed%20up%20/%20slow%20down%20a%20video
+  if (speed) {
+    videoFilters.push(`setpts=(1/${1 / speed})*PTS`)
+    audioFilters.push(`atempo=${speed}`)
   }
 
   // https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
   if (format === 'gif') {
-    filters.push(
+    videoFilters.push(
       `split [a][b];[a] palettegen [palette];[b][palette] paletteuse`
     )
   }
 
-  if (filters.length !== 0) {
-    result.push({ filter_complex: `"[0:v] ${filters.join(',')}"` })
+  if (videoFilters.length !== 0) {
+    result.push({ filter_complex: `"[0:v] ${videoFilters.join(',')}"` })
+  }
+  if (audioFilters.length !== 0) {
+    result.push({ 'filter:a': `"${audioFilters.join(',')}"` })
   }
   return result
 }
