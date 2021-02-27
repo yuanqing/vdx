8ff04782f46f31ade0011be6d50e629f437bd9ee
yuanqing <desk@yuanqing.sg>
2021-02-27 15:39:34 +0800

    `resolve(null)`

--- src/execute-shell-commands.ts
+++ src/execute-shell-commands.ts
@@ -34,7 +34,7 @@ export async function executeShellCommands(
               '›'
             )} ${escapeFilePath(outputFile)}`
           )
-          resolve()
+          resolve(null)
         })
       })
     }
